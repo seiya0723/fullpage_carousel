13fa9c8dc1ba1ec981739bce892c7a64af6e3ca4
Seiya Asahina <seiya@asahina>
2020-08-20 13:15:31 +0900

    全画面カルーセル

---
+++ onload.js
@@ -0,0 +1,13 @@
+window.addEventListener("load" , function (){
+
+    //ここでスライドショーを表示する
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:6000,
+        arrows:false,
+        
+    
+    });
+
+});
+
